4d5750c5ab6bf23bcf49ea41b08a54221faf95e1
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-27 23:13:07 +0530

    challenge 3 --> Profile form generator done

--- JAVASCRIPT/DOM/Dom Challenge Assignment/challenge-3/app.js
+++ JAVASCRIPT/DOM/Dom Challenge Assignment/challenge-3/app.js
@@ -1,3 +1,17 @@
 /**
  * Write your challenge solution here
  */
+const namee = document.getElementById("nameInput");
+const age = document.getElementById("ageInput");
+const job = document.getElementById("jobInput");
+const bio = document.getElementById("bioInput");
+
+const nameDisplay = document.getElementById("nameDisplay");
+const ageDisplay = document.getElementById("ageDisplay");
+const bioDisplay = document.getElementById("bioDisplay");
+const jobDisplay = document.getElementById("jobDisplay");
+
+namee.addEventListener("input", () => (nameDisplay.innerText = namee.value));
+age.addEventListener("input", () => (ageDisplay.innerText = age.value));
+job.addEventListener("input", () => (jobDisplay.innerText = job.value));
+bio.addEventListener("input", () => (bioDisplay.innerText = bio.value));
