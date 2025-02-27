e3d2c5629e5bbd5081329da64e4c9f1f3138e0a0
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-27 22:23:46 +0530

    toggle bulb project done

--- JAVASCRIPT/DOM/Dom Challenge Assignment/challenge-1/app.js
+++ JAVASCRIPT/DOM/Dom Challenge Assignment/challenge-1/app.js
@@ -1,3 +1,30 @@
 /**
  * Write your challenge solution here
  */
+let statusButton = document.getElementById("status");
+let toggleButton = document.getElementById("toggleButton");
+let bulb = document.getElementById("bulb");
+let body = document.getElementById("body");
+function lighton() {
+  body.style.color = "white";
+  body.style.backgroundColor = "black";
+  toggleButton.innerText = "Turn Off";
+  statusButton.innerText = "Status: on";
+  bulb.classList.toggle("bulbon");
+}
+function lightoff(){
+  body.style.color = "black";
+  body.style.backgroundColor = "white";
+  toggleButton.innerText = "Turn On";
+  statusButton.innerText = "Status: off";
+  bulb.classList.toggle("bulbon");
+
+}
+
+toggleButton.addEventListener("click", function () {
+  if (toggleButton.innerText === "Turn On") {
+    lighton();
+  }else{
+    lightoff();
+  }
+});
