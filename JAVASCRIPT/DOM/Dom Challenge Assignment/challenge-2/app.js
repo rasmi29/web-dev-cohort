ede84d10cb448d3eb819440b25c82ba9159dceca
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-27 22:32:49 +0530

    challenge 2 --> text color changer

--- JAVASCRIPT/DOM/Dom Challenge Assignment/challenge-2/app.js
+++ JAVASCRIPT/DOM/Dom Challenge Assignment/challenge-2/app.js
@@ -1,3 +1,31 @@
 /**
  * Write your challenge solution here
  */
+
+const red = document.getElementById('redButton');
+const green = document.getElementById('greenButton');
+const blue = document.getElementById('blueButton');
+const purple = document.getElementById('purpleButton');
+const reset = document.getElementById('resetButton');
+
+const heading = document.getElementById("mainHeading");
+
+red.addEventListener("click", function (){
+    heading.style.color = "red";
+});
+
+green.addEventListener("click", function (){
+    heading.style.color = "green";
+})
+
+blue.addEventListener("click", function (){
+    heading.style.color = "blue";
+})
+
+purple.addEventListener("click", function (){
+    heading.style.color = "purple";
+})
+
+reset.addEventListener("click", function (){
+    heading.style.color = "black";
+})
