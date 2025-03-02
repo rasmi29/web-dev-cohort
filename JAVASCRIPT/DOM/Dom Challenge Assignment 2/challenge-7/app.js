a64e9895ddaa2ad9e470ad86ed2c7bf29abaa656
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-03-03 01:02:47 +0530

    Accordion Project Done

--- JAVASCRIPT/DOM/Dom Challenge Assignment 2/challenge-7/app.js
+++ JAVASCRIPT/DOM/Dom Challenge Assignment 2/challenge-7/app.js
@@ -1,3 +1,15 @@
-/**
- * Write your challenge solution here
- */
+const accordion_item = document.querySelectorAll(".accordion-item");
+const accordion_button = document.querySelectorAll(".accordion-button");
+const arrow = document.querySelectorAll(".arrow");
+const accordion_content = document.querySelectorAll(".accordion-content");
+
+accordion_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+    } else {
+      accordion_item.forEach((item) => item.classList.remove("active"));
+      item.classList.toggle("active");
+    }
+  });
+});
